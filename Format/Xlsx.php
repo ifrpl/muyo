f0501da515427b645b384965e3d8d95398f49818
lukasz <lukasz@grzana.pl>
2012-08-27 07:07:26 +0200

    export

--- Format/Xlsx.php
+++ Format/Xlsx.php
@@ -10,10 +10,10 @@ class IFR_Main_Format_Xlsx extends IFR_Main_Format
 
 	public function __construct()
 	{
-		if(!PHPExcel_Settings::setCacheStorageMethod(PHPExcel_CachedObjectStorageFactory::cache_to_discISAM))
-	    {
-		    throw new Exception('Problem with creating cache');
-	    }
+			if(!PHPExcel_Settings::setCacheStorageMethod(PHPExcel_CachedObjectStorageFactory::cache_to_discISAM))
+		    {
+			    throw new Exception('Problem with creating cache');
+		    }
 
 		$this->phpexcelObj = new PHPExcel();
 		$this->phpexcelObj->getProperties()->setTitle( $this->getTitle() );
@@ -36,7 +36,7 @@ class IFR_Main_Format_Xlsx extends IFR_Main_Format
 		$this->rowIndex++;
 	}
 
-	public function addRow($row)
+	public function addRow(array $row)
 	{
 		$this->phpexcelObj->setActiveSheetIndex(0);
 		$sheet = $this->phpexcelObj->getActiveSheet();
