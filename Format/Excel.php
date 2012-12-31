62177dfcceb52daa027783e0a8b9263856885a2f
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2012-12-31 12:34:26 +0100

    PHPExcel path fix

--- Format/Excel.php
+++ Format/Excel.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'IFR/PHPexcel/Classes/PHPExcel/Classes/PHPExcel.php';
+require_once ROOT_PATH.'/library/IFR/PHPexcel/Classes/PHPExcel/Classes/PHPExcel.php';
 
 class IFR_Main_Format_Excel extends IFR_Main_Format
 {
