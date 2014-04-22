8bf6b76ba534338764038d7e822477019068991e
grzana <lukasz@gembit.pl>
2014-04-22 21:34:17 +0200

    include model condition

--- model/_.php
+++ model/_.php
@@ -1,7 +1,13 @@
 <?php
 
-require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'model.php') ) );
-require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'set.php') ) );
+if( !class_exists( 'Lib_Model' ) )
+{
+	require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'model.php') ) );
+}
+if( !class_exists( 'Lib_Model_Set' ) )
+{
+	require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'set.php') ) );
+}
 
 require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'array','_.php') ) );
 require_once( implode( DIRECTORY_SEPARATOR, array(__DIR__,'db','_.php') ) );
