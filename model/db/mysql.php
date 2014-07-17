f409943a631ac3a4a6b1dc18724668f5fb8fe6f4
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2014-07-17 11:57:10 +0200

    loadString - better _join_ handling

--- model/db/mysql.php
+++ model/db/mysql.php
@@ -807,14 +807,14 @@ abstract class Lib_Model_Db_Mysql extends Lib_Model_Db
 	 */
 	public function loadString()
 	{
-		$alias = $this->getAlias();
 		$array = $this->loadArray( null,true );
 		debug_enforce_count_gte( $array, 1 );
 		debug_assert_count_eq( $array, 1 );
 		$record = array_shift( $array );
-		debug_enforce_count_gte( $record[ $alias ], 1 );
-		debug_assert_count_eq( $record[ $alias ], 1 );
-		$ret=array_shift( $record[ $alias ] );
+		$columns = array_flatten( $record );
+		debug_enforce_count_gte( $columns, 1 );
+		debug_assert_count_eq( $columns, 1 );
+		$ret=array_shift( $columns );
 		return $ret;
 	}
 
