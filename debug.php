9c4f0981ef718d697bcb409a5c07bb3730a64c38
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2015-04-09 14:10:21 +0200

    move zend log-level checking to zend file

--- debug.php
+++ debug.php
@@ -567,29 +567,6 @@ if( !function_exists('debug_enforce') )
 	}
 }
 
-if( class_exists('Zend_Log') )
-{
-	debug_assert(function() {
-		foreach(array(
-			LOG_EMERG   => Zend_Log::EMERG,
-			LOG_ALERT   => Zend_Log::ALERT,
-			LOG_CRIT    => Zend_Log::CRIT,
-			LOG_ERR     => Zend_Log::ERR,
-			LOG_WARNING => Zend_Log::WARN,
-			LOG_NOTICE  => Zend_Log::NOTICE,
-			LOG_INFO    => Zend_Log::INFO,
-			LOG_DEBUG   => Zend_Log::DEBUG
-		) as $k => $v)
-		{
-			if( $k != $v )
-			{
-				return false;
-			}
-		}
-		return true;
-	}, 'One of log levels differ. Update the library.');
-}
-
 if( !function_exists('debug_trace_func_call') )
 {
 	/**
