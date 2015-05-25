1aaea50e694b312a94c6f55c2ec92c7c3bf64875
grzana <lukasz@gembit.pl>
2015-05-25 12:08:03 +0200

    getGridConfig() hidden field

--- model/model.php
+++ model/model.php
@@ -1687,6 +1687,9 @@ abstract class Lib_Model implements Iterator
 						)), true);
 
 						break;
+					case "hidden":
+						//noting to set
+						break;
 					default:
 						debug_assert(false !== array_search($optionValue->type, self::$types), "Unknown Grid Cell Type `{$optionValue->type}`");
 				}
