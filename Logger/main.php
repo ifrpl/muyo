7e5091a78a14e6b11113b0155a3aa09eef03317d
root <mikolaj.okroj@ifresearch.org>
2015-06-16 17:40:08 +0200

    Revert "Logger"
    This reverts commit fa74a6c058e1f97d0172cc014d1019ff5a70d8f0.

--- Logger/main.php
+++ Logger/main.php
@@ -56,4 +56,56 @@ if( !function_exists('logger_main') )
 
         };
     }
+
+    function log_mail( $mailingList, $msg, $level )
+    {
+        $host = gethostname();
+        if( array_key_exists( 'HTTP_HOST', $_SERVER ) && $_SERVER['HTTP_HOST'] )
+        {
+            $host = $_SERVER[ 'HTTP_HOST' ];
+        }
+
+        mail( $mailingList, log_level_str($level) . ' on ' . $host, $msg );
+    }
+
+    /**
+     * Logging to standard output
+     *
+     * @param string $msg
+     * @param int $level PHP log levels
+     */
+    function log_output( $msg, $level )
+    {
+        writeln( $msg );
+    }
+
+    /**
+     * Logging to syslog
+     *
+     * @param string $msg
+     * @param int $level PHP log levels
+     */
+    function log_syslog( $msg, $level )
+    {
+        $identity = '';
+
+        if( defined('HOST_GLOBAL') )
+        {
+            $identity .= HOST_GLOBAL;
+        }
+        else
+        {
+            $identity .= basename(ROOT_PATH);
+        }
+
+        openlog($identity, LOG_PID | LOG_ODELAY, LOG_USER);
+
+        foreach( explode("\n", $msg) as $row )
+        {
+            syslog($level, str_replace("\t", '    ', $row));
+        }
+    }
+
+
+
 }
