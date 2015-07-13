dd19f3c9aaeaeb0a977c7cf7120732e6944d14b4
grzana <lukasz@gembit.pl>
2015-07-13 12:15:22 +0200

    is_serialized conditionaly

--- is_serialized.php
+++ is_serialized.php
@@ -1,108 +1,87 @@
 <?php
 
-if(!function_exists('is_serialized'))
+if( !function_exists( 'is_serialized' ) )
 {
 	/**
-	 * Tests if an input is valid PHP serialized string.
+	 * @param      $data
+	 * @param bool $strict
 	 *
-	 * Checks if a string is serialized using quick string manipulation
-	 * to throw out obviously incorrect strings. Unserialize is then run
-	 * on the string to perform the final verification.
-	 *
-	 * Valid serialized forms are the following:
-	 * <ul>
-	 * <li>boolean: <code>b:1;</code></li>
-	 * <li>integer: <code>i:1;</code></li>
-	 * <li>double: <code>d:0.2;</code></li>
-	 * <li>string: <code>s:4:"test";</code></li>
-	 * <li>array: <code>a:3:{i:0;i:1;i:1;i:2;i:2;i:3;}</code></li>
-	 * <li>object: <code>O:8:"stdClass":0:{}</code></li>
-	 * <li>null: <code>N;</code></li>
-	 * </ul>
-	 *
-	 * @author		Chris Smith <code+php@chris.cs278.org>
-	 * @copyright	Copyright (c) 2009 Chris Smith (http://www.cs278.org/)
-	 * @license		http://sam.zoy.org/wtfpl/ WTFPL
-	 * @param		string	$value	Value to test for serialized form
-	 * @param		mixed	$result	Result of unserialize() of the $value
-	 * @return		boolean			True if $value is serialized data, otherwise false
+	 * @return bool
 	 */
-	function is_serialized($value, &$result = null)
+	function is_serialized( $data, $strict = true )
 	{
-		// Bit of a give away this one
-		if (!is_string($value))
+		// if it isn't a string, it isn't serialized.
+		if( !is_string( $data ) )
 		{
 			return false;
 		}
-
-		// Serialized false, return true. unserialize() returns false on an
-		// invalid string or it could return false if the string is serialized
-		// false, eliminate that possibility.
-		if ($value === 'b:0;')
+		$data = trim( $data );
+		if( 'N;'==$data )
 		{
-			$result = false;
 			return true;
 		}
-
-		$length	= strlen($value);
-		$end	= '';
-
-		switch ($value[0])
+		if( strlen( $data )<4 )
 		{
-			case 's':
-				if ($value[$length - 2] !== '"')
-				{
-					return false;
-				}
-			case 'b':
-			case 'i':
-			case 'd':
-				// This looks odd but it is quicker than isset()ing
-				$end .= ';';
-			case 'a':
-			case 'O':
-				$end .= '}';
-
-				if ($value[1] !== ':')
-				{
-					return false;
-				}
-
-				switch ($value[2])
-				{
-					case 0:
-					case 1:
-					case 2:
-					case 3:
-					case 4:
-					case 5:
-					case 6:
-					case 7:
-					case 8:
-					case 9:
-						break;
-
-					default:
-						return false;
-				}
-			case 'N':
-				$end .= ';';
-
-				if ($value[$length - 1] !== $end[0])
-				{
-					return false;
-				}
-				break;
-
-			default:
-				return false;
-		}
-
-		if (($result = @unserialize($value)) === false)
-		{
-			$result = null;
 			return false;
 		}
-		return true;
+		if( ':'!==$data[ 1 ] )
+		{
+			return false;
+		}
+		if( $strict )
+		{
+			$lastc = substr( $data, -1 );
+			if( ';'!==$lastc && '}'!==$lastc )
+			{
+				return false;
+			}
+		}
+		else
+		{
+			$semicolon = strpos( $data, ';' );
+			$brace = strpos( $data, '}' );
+			// Either ; or } must exist.
+			if( false===$semicolon && false===$brace )
+			{
+				return false;
+			}
+			// But neither must be in the first X characters.
+			if( false!==$semicolon && $semicolon<3 )
+			{
+				return false;
+			}
+			if( false!==$brace && $brace<4 )
+			{
+				return false;
+			}
+		}
+		$token = $data[ 0 ];
+		switch( $token )
+		{
+			case 's' :
+				if( $strict )
+				{
+					if( '"'!==substr( $data, -2, 1 ) )
+					{
+						return false;
+					}
+				}
+				elseif( false===strpos( $data, '"' ) )
+				{
+					return false;
+				}
+			// or else fall through
+			case 'a' :
+			case 'O' :
+				return (bool)preg_match( "/^{$token}:[0-9]+:/s", $data );
+			case 'b' :
+			case 'i' :
+			case 'd' :
+				$end = $strict ? '$' : '';
+
+				return (bool)preg_match( "/^{$token}:[0-9.E-]+;$end/", $data );
+		}
+
+		return false;
 	}
 }
